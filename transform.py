bf1a894b2e1860be2a0be454c44d939e46fc3f31
宮戸 岳 <takeru.miyato@gmail.com>
2017-12-13 04:24:35 +0900

    add rotation function

--- transform.py
+++ transform.py
@@ -23,19 +23,17 @@ def transform(imgs, depthes, poses, K):
     if H != depthes.shape[2] or W != depthes.shape[3]:
         raise Exception("Height and Width of images and depthes should be the same each other")
 
-    p_t = utils.generate_meshgrid(H, W, xp) # Generate target coordinate
-    Kinvp = F.matmul(F.batch_inv(K), p_t.reshape(3, H*W)).reshape(N, 3, H, W) # TODO: dimemsion might be mismatched
+    p_t = utils.generate_2dmeshgrid(H, W, xp)  # Generate target coordinate
+    Kinvp = F.matmul(F.batch_inv(K), p_t.reshape(3, H * W)).reshape(N, 3, H, W)  # TODO: dimemsion might be mismatched
     DKinvp = depthes * Kinvp
-    DKinvp = F.concat([DKinvp, xp.ones(N, 1, H, W, xp.float32)], axis=1) # Convert to homgeneneous vectors
-    T = utils.pose_vec2mat(poses)
-    filter = xp.tile(xp.asarray([0.0, 0.0, 0.0, 1.0], xp.float32).reshape(1,1,4), [N, 1, 1])
-    _K = F.concat(
-        [F.concat([K, xp.zeros([N, 3, 1], xp.float32)], axis=2), filter], axis=1)
-    KT = F.matmul(_K, T)
-    p_s = F.matmul(KT, F.reshape(DKinvp, (N, 4, -1))) # Source coordinate
+    DKinvp = F.concat([DKinvp, xp.ones(N, 1, H, W, xp.float32)], axis=1)  # Convert to homgeneneous vectors
+    T = utils.pose_vec2mat(poses, xp)
+    filter = xp.tile(xp.asarray([0.0, 0.0, 0.0, 1.0], xp.float32).reshape(1, 1, 4), [N, 1, 1])
+    _K = F.concat([F.concat([K, xp.zeros([N, 3, 1], xp.float32)], axis=2), filter], axis=1)
+    KT = F.batch_matmul(_K, T)
+    p_s = F.batch_matmul(KT, F.reshape(DKinvp, (N, 4, -1)))  # Source coordinate
     p_s_xy = p_s[:, 0:2, :]
     p_s_xy /= F.broadcast_to(p_s[:, 2:3, :], p_s.shape) + 1e-10
     p_s_xy = F.reshape(p_s_xy, (N, 2, H, W))
     transformed_img = F.spatial_transformer_sampler(imgs, p_s_xy)
     return transformed_img
-
--- utils.py
+++ utils.py
@@ -1,15 +1,38 @@
 import numpy as np
+from chainer import functions as F
 
 
-def euler2mat(z, y, x):
-    raise NotImplementedError
+def getG(r, xp):
+    N = r.shape[0]
+    G = xp.zeros(N, 3, 3)
+    G[:, 1, 2] = r[:, 0]
+    G[:, 2, 1] = -r[:, 0]
+    G[:, 2, 0] = r[:, 1]
+    G[:, 0, 2] = -r[:, 1]
+    G[:, 0, 1] = r[:, 2]
+    G[:, 1, 0] = -r[:, 2]
 
 
-def pose_vec2mat(vec):
-    raise NotImplementedError
+# rod rotation formula
+def r2mat(r, xp):
+    # Matrix exponential
+    N = r.shape[0]
+    I = xp.tile(xp.eye(3).reshape(1, 3, 3), (N, 1, 1))
+    theta = r / F.sqrt(F.sum(r ** 2, axis=1, keepdims=True))
+    G = getG(r, xp)
+    return I + F.sin(theta) * G + (1 - F.cos(theta)) * F.batch_matmul(G, G)
 
 
-def generate_meshgrid(H, W, xp=np):
+def pose_vec2mat(vec, xp):
+    r, t = vec[:, :3], vec[:, 3:]
+    N = r.shape[0]
+    R = r2mat(r, xp)
+    zzzo = xp.tile(xp.asarray([0.0, 0.0, 0.0, 1.0], xp.float32).reshape(1, 1, 4), [N, 1, 1])
+    Rt = F.concat([F.concat([R, t.reshape(N, 1, 3)], axis=2), zzzo], axis=1)
+    return Rt
+
+
+def generate_2dmeshgrid(H, W, xp=np):
     ys, xs = xp.meshgrid(
         xp.linspace(-1, 1, H, dtype=np.float32),
         xp.linspace(-1, 1, W, dtype=np.float32), indexing='ij',
