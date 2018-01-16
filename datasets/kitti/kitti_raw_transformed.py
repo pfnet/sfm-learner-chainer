94c31517a36f881758e23bc323e7c2ed0dfeb888
yukitsuji <yukitsuji020832@gmail.com>
2018-01-16 17:51:42 +0900

    Complete data augmentation

--- datasets/kitti/kitti_raw_transformed.py
+++ datasets/kitti/kitti_raw_transformed.py
@@ -11,16 +11,13 @@ import copy
 
 from chainer import datasets
 from datasets.kitti.kitti_raw_dataset import KittiRawDataset
+from chainer import functions as F
+
 
 def make_intrinsics_matrix(fx, fy, cx, cy):
-    # Assumes batch input
-    batch_size = fx.get_shape().as_list()[0]
-    zeros = tf.zeros_like(fx)
-    r1 = tf.stack([fx, zeros, cx], axis=1)
-    r2 = tf.stack([zeros, fy, cy], axis=1)
-    r3 = tf.constant([0.,0.,1.], shape=[1, 3])
-    r3 = tf.tile(r3, [batch_size, 1])
-    intrinsics = tf.stack([r1, r2, r3], axis=1)
+    intrinsics = np.array([[fx, 0., cx],
+                           [0., fy, cy],
+                           [0., 0., 1.]], dtype='f')
     return intrinsics
 
 def data_augmentation(tgt_img, src_imgs, intrinsics):
@@ -28,45 +25,53 @@ def data_augmentation(tgt_img, src_imgs, intrinsics):
 
        Args:
            tgt_img(ndarray): Shape is (3, H, W)
-           src_img(list): Shape is [N, 3, H, W]
+           src_img(list): Shape is [S, 3, H, W]
            intrinsics(ndarray): Shape is (3, 3)
     """
     # Random scaling
-    def random_scaling(im, intrinsics):
-        batch_size, in_h, in_w, _ = im.get_shape().as_list()
+    def random_scaling(imgs, intrinsics):
+        batch_size, _, in_h, in_w = imgs.shape
         scaling = np.random.uniform(1, 1.15, 2)
         x_scaling = scaling[0]
         y_scaling = scaling[1]
-        out_h = tf.cast(in_h * y_scaling, dtype=tf.int32)
-        out_w = tf.cast(in_w * x_scaling, dtype=tf.int32)
-        im = tf.image.resize_area(im, [out_h, out_w])
-        fx = intrinsics[:,0,0] * x_scaling
-        fy = intrinsics[:,1,1] * y_scaling
-        cx = intrinsics[:,0,2] * x_scaling
-        cy = intrinsics[:,1,2] * y_scaling
+        out_h = int(in_h * y_scaling)
+        out_w = int(in_w * x_scaling)
+        imgs = F.resize_images(imgs, [out_h, out_w]).data
+        fx = intrinsics[0,0] * x_scaling
+        fy = intrinsics[1,1] * y_scaling
+        cx = intrinsics[0,2] * x_scaling
+        cy = intrinsics[1,2] * y_scaling
         intrinsics = make_intrinsics_matrix(fx, fy, cx, cy)
-        return im, intrinsics
+        return imgs, intrinsics
 
     # Random cropping
-    def random_cropping(im, intrinsics, out_h, out_w):
-        # batch_size, in_h, in_w, _ = im.get_shape().as_list()
-        batch_size, in_h, in_w, _ = tf.unstack(tf.shape(im))
-        offset_y = tf.random_uniform([1], 0, in_h - out_h + 1, dtype=tf.int32)[0]
-        offset_x = tf.random_uniform([1], 0, in_w - out_w + 1, dtype=tf.int32)[0]
-        im = tf.image.crop_to_bounding_box(
-            im, offset_y, offset_x, out_h, out_w)
-        fx = intrinsics[:,0,0]
-        fy = intrinsics[:,1,1]
-        cx = intrinsics[:,0,2] - tf.cast(offset_x, dtype=tf.float32)
-        cy = intrinsics[:,1,2] - tf.cast(offset_y, dtype=tf.float32)
+    def random_cropping(imgs, intrinsics, out_h, out_w):
+        batch_size, _, in_h, in_w = imgs.shape
+        offset_y = int(np.random.uniform(1, 0, in_h - out_h + 1)[0])
+        offset_x = int(np.random.uniform(1, 0, in_w - out_w + 1)[0])
+        imgs = imgs[:, :, offset_y:offset_y+out_h, offset_x:offset_x+out_w]
+        fx = intrinsics[0,0]
+        fy = intrinsics[1,1]
+        cx = intrinsics[0,2] - offset_x
+        cy = intrinsics[1,2] - offset_y
         intrinsics = make_intrinsics_matrix(fx, fy, cx, cy)
-        return im, intrinsics
+        return imgs, intrinsics
+
+    # Random flip
+    def random_flip(imgs, intrinsics):
+        batch_size, _, in_h, in_w = imgs.shape
+        if np.random.rand() < 0.5:
+            imgs = imgs[:, :, :, ::-1]
+            intrinsics[0, 2] = in_w - intrinsics[0, 2]
+        return imgs, intrinsics
 
     _, out_h, out_w = tgt_img.shape
-    im, intrinsics = random_scaling(im, intrinsics)
-    im, intrinsics = random_cropping(im, intrinsics, out_h, out_w)
-    im = tf.cast(im, dtype=tf.uint8)
-    return im, intrinsics
+    imgs = np.concatenate((tgt_img[np.newaxis, :], src_imgs))
+    imgs, intrinsics = random_scaling(imgs, intrinsics)
+    imgs, intrinsics = random_cropping(imgs, intrinsics, out_h, out_w)
+    imgs, intrinsics = random_flip(imgs, intrinsics)
+    # im = tf.cast(im, dtype=tf.uint8)
+    return imgs[0], [img for img in imgs[1:]], intrinsics
 
 def get_multi_scale_intrinsics(intrinsics, n_scales):
     """Scale the intrinsics accordingly for each scale
@@ -84,39 +89,25 @@ def get_multi_scale_intrinsics(intrinsics, n_scales):
         cy = intrinsics[1, 2]/(2 ** s)
         intrinsics = np.array([[fx, 0., cx],
                                [0., fy, cy],
-                               [0., 0., 1.]])
+                               [0., 0., 1.]], dtype='f')
         multi_intrinsics.append(intrinsics)
     return multi_intrinsics
 
 def _transform(inputs, n_scale=4, ):
-    tgt_img, reg_imgs, intrinsics, inv_intrinsics = inputs
+    tgt_img, src_imgs, intrinsics, inv_intrinsics = inputs
     del inputs
-
-    # # Global scaling
-    # if g_scale:
-    #     scale = np.random.uniform(g_scale[0], g_scale[1], 4)
-    #     pc *= scale
-    #     places *= scale[:3]
-    #     size *= scale[:3]
-
-    # # Flip
-    # if fliplr:
-    #     if np.random.rand() > 0.5:
-    #         pc[:, 1] = pc[:, 1] * -1
-    #         places[:, 1] = places[:, 1] * -1
-    #         rotates = rotates * -1
 
     tgt_img, src_imgs, intrinsics = data_augmentation(tgt_img, src_imgs,
                                                       intrinsics)
     intrinsics = get_multi_scale_intrinsics(intrinsics, n_scale)
-    return tgt_img, src_imgs, intrinsics, _
+    return tgt_img, src_imgs, intrinsics, intrinsics
 
 
 class KittiRawTransformed(datasets.TransformDataset):
     def __init__(self, data_dir=None, seq_len=3, split='train',
                  n_scale=4, ):
         self.d = KittiRawDataset(
-            data_dir=None, seq_len=3, split='train')
+            data_dir=data_dir, seq_len=seq_len, split=split)
         t = partial(
             _transform, n_scale=4, )
         super().__init__(self.d, t)
